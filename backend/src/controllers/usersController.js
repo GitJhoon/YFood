124651edcb07baff1c4473a805b07c6223789805
GitJhoon <jonathan22office@gmail.com>
2026-04-19 00:06:32 -0300

    Atualizando o CRUD

--- backend/src/controllers/usersController.js
+++ backend/src/controllers/usersController.js
@@ -1,6 +1,7 @@
 import Client from "../database/mongodb.js";
 import { successResponse, errorResponse, notFoundResponse } from "../helpers/httpResponse.js";
 import { ObjectId } from "mongodb";
+import crypto from "crypto";
 
 export default class  usersController {
     constructor(){
@@ -27,16 +28,27 @@ export default class  usersController {
         }
     }
 
-    async updateUser(id, dataUser){
+    async updateUser(id, userData){
         try{
+            if(userData.password){
+                const salt = crypto.randomBytes(16);
+
+                crypto.pbkdf2(userData.password, salt, 31000, 16, "sha256", async (error, hashedPassword) => {
+                    if(error){
+                        throw error;
+                    }
+                    
+                    userData = { ...userData, password: hashedPassword, salt: salt};
+                });
+
+            }
             const updatedUser = await Client.db.collection(this.collectionName)
-            .findOneAndUpdate({_id: new ObjectId(id)}, {$set: dataUser});
+            .findOneAndUpdate({_id: new ObjectId(id)}, {$set: userData});
             
             return successResponse(updatedUser);
         }catch(error){
             return errorResponse(error);
         }
     }
-
 
 }
